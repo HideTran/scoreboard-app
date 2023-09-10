ceb2b3c4ad2abf1c6db7d58f8972b2d1624fac42
HideTran <thanhhai071289@gmail.com>
2023-09-10 17:55:02 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,60 +5,76 @@ function App() {
     {
       id: Date.now() * Math.random(),
       name: "Nguyễn A",
-      quantity: 0
+      // quantity: 0
     }
   ])
 
-  function addList(e){
+  function addList(e) {
     e.preventDefault();
     let newDo = {
       id: Date.now() * Math.random(),
       name: e.target.name.value,
-      quantity: 0
+      // quantity: 0
     }
     setDoList([...doList, newDo])
   }
 
   function check(doId) {
     setDoList(doList.map(doItem => {
-      if(doItem.id == doId) {
+      if (doItem.id == doId) {
         doItem.name = !doItem.name
       }
       return doItem
     }))
   }
-  
+
   function deleteDo(doId) {
     setDoList(doList.filter(doItem => doItem.id != doId))
   }
+
+  const [count, setCount] = useState(0)
+  const Counter = ({ count, setCount }) => {
+    const increase = () => {
+      setCount(count + 1);
+    };
+    return
+    const decrease = () => {
+      setCount(count - 1);
+    };
+    return
+  };
 
   return (
     <>
       <h1>Bảng Điểm</h1>
       <ul>
-      {
+        {
           doList.map((doItem) => (
             <li key={doItem.id}>
-          <button onClick={() => {
-            deleteDo(doItem.id)
-          }}>x</button>
-          <input onChange={() => {
-            check(doItem.id)
-          }} type="hidden"/>
-          <span>{doItem.name}</span>
-          <button onClick={() => {
-
-          }}>-</button>
-          <button>{doItem.quantity}</button>
-          <button>+</button>
-        </li>
+              <button onClick={() => {
+                deleteDo(doItem.id)
+              }}>x</button>
+              <input onChange={() => {
+                check(doItem.id)
+              }} type="hidden" />
+              <span>{doItem.name}</span>
+              <span className="btn-count">
+                <button onClick={() => {
+                  Counter(decrease)
+                }}>-</button>
+                <button>{count}</button>
+                <button onClick={() => {
+                  Counter(increase)
+                }}>+</button>
+              </span>
+            </li>
           ))
         }
       </ul>
       <form onSubmit={(e) => {
         addList(e)
       }}>
-        <input name="name" type="text" placeholder="Enter a player's name"/>
+        <input name="name" type="text" placeholder="Enter a player's name" />
         <button>Add Player</button>
       </form>
 
